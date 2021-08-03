6a3e232b3780d6dcb9e0407c0fe6e195b34a0c0c
LYejin <vgg789@naver.com>
2021-08-03 20:49:37 +0900

    typescript

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const person = {
+    name: "nicolas",
+    age: 22,
+    gender: "male",
+};
+const sayHi = (person) => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+console.log(sayHi(person));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "nicolas",
+  age: 22,
+  gender: "male",
+};
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(person));
+
+export {};
